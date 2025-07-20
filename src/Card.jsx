cb5f46428ef1b36d5fd119b0579c24cda2d08204
narehiko <galihsaiyan@gmail.com>
2025-07-20 23:48:07 +0700

    Update

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,11 @@
-import picture from './assets/me.png'
+import picture from './assets/me.webp'
 import './index.css'
 
 function Card() {
     return(
         <div className='card background'>
             <a href="https://github.com/narehiko/Card-Components-React-JS">
-            <img className='profilePic' src={picture} alt="profile picture"></img>
+            <img className='profilePic' loading='lazy' src={picture} alt="profile picture"></img>
             </a>
             <h2 className='card-title'>narehiko</h2>
             <p className='card-text'>I'm currently learning React 💕</p>
